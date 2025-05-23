7305922d23a9ec0e94d32babebb7e5a1988f93e4
arlaspresident <jankovic4901@gmail.com>
2025-05-23 09:53:35 +0200

    importerade middleware och skyddade de routes som borde kräva inloggning

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const menuController = require('../controllers/menuController');
+const verifyToken = require('../middleware/verifyToken');
+
 
 // get/api/menu
 router.get('/', menuController.getMenu);
 
 // post /api/menu
-router.post('/', menuController.addMenuItem);
-
-router.put('/:id', menuController.updateMenuItem);
-router.delete('/:id', menuController.deleteMenuItem);
+router.post('/', verifyToken, menuController.addMenuItem);
+router.put('/:id', verifyToken, menuController.updateMenuItem);
+router.delete('/:id', verifyToken, menuController.deleteMenuItem);
 
 module.exports = router;
